19d379f21cd9e74da6bb8c73c4e1274f55bf426c
Zeeshanalik <zeeshan.ali@datumbrain.com>
2021-03-24 17:39:36 +0500

    Created a Generic Function To Make an API Request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
+import React, { useEffect } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const onRequest = ({ method = 'GET', url, header = {}, data = {} }) =>
+    axios.request({
+      method: method,
+      url: url,
+      header: header,
+      data: data,
+    });
+  useEffect(() => {
+    const params = {
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/users/',
+    };
+    onRequest(params).then((response) => {
+      console.log(response?.data ?? []);
+    });
+  }, []);
+  return <> </>;
 }
 
 export default App;
